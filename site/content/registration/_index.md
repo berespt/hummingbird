cb76541e6c94effc4a32119aea4733e45e7f0d35
Piotr Beres <piotr.beres@teamaol.com>
2018-02-03 01:30:18 +0100

    Simplify registration page

--- site/content/registration/_index.md
+++ site/content/registration/_index.md
@@ -43,32 +43,6 @@ intro:
     java’s origin and support the farmers that grew it. We take coffee
     production, roasting and brewing seriously and we’re glad to pass that
     knowledge to anyone. This is an edit via identity...
-main:
-  heading: Great coffee with no compromises
-  description: >
-    We hold our coffee to the highest standards from the shrub to the cup.
-    That’s why we’re meticulous and transparent about each step of the coffee’s
-    journey. We personally visit each farm to make sure the conditions are
-    optimal for the plants, farmers and the local environment.
-  image1:
-    alt: A close-up of a paper filter filled with ground coffee
-    image: /img/registration-grid3.jpg
-  image2:
-    alt: A green cup of a coffee on a wooden table
-    image: /img/registration-grid2.jpg
-  image3:
-    alt: Coffee beans
-    image: /img/registration-grid1.jpg
-testimonials:
-  - author: Elisabeth Kaurismäki
-    quote: >-
-      The first time I tried Kaldi’s coffee, I couldn’t even believe that was
-      the same thing I’ve been drinking every morning.
-  - author: Philipp Trommler
-    quote: >-
-      Kaldi is the place to go if you want the best quality coffee. I love their
-      stance on empowering farmers and transparency.
-full_image: /img/registration-full-width.jpg
 pricing:
   heading: Monthly subscriptions
   description: >-
--- src/js/cms-preview-templates/registration.js
+++ src/js/cms-preview-templates/registration.js
@@ -30,41 +30,6 @@ export default class PostPreview extends React.Component {
         </div>
       </div>
 
-      <div className="mw7 center">
-        <div className="mw6 ph3 mb3">
-          <h3 className="f3 b lh-title mb2">{entry.getIn(["data", "main", "heading"])}</h3>
-          <p>{entry.getIn(["data", "main", "description"])}</p>
-        </div>
-      </div>
-
-      <div className="mw7 center ph3 pv4">
-
-        <div className="flex flex-wrap mhn1">
-          <div className="w-100 w-50-ns ph1-ns">
-            <img src={getAsset(entry.getIn(["data", "main", "image1", "image"]))}/>
-          </div>
-
-          <div className="w-100 w-50-ns ph1-ns">
-            <img src={getAsset(entry.getIn(["data", "main", "image2", "image"]))}/>
-          </div>
-
-          <div className="w-100 ph1-ns">
-            <img src={getAsset(entry.getIn(["data", "main", "image3", "image"]))}/>
-          </div>
-        </div>
-      </div>
-
-      <div className="pb4">
-        {(entry.getIn(['data', 'testimonials']) || []).map((testimonial, index) => <div className="center mb3 ph3" key={index}>
-        	<blockquote className="bg-grey-1 primary pa3 mb3 br1 b mw6 center">
-        		<p className="f4 mb0">“{testimonial.get('quote')}”</p>
-        		<cite className="tr db grey-3">{testimonial.get('author')}</cite>
-        	</blockquote>
-        </div>)}
-      </div>
-
-      <img src={getAsset(entry.getIn(['data', 'full_image']))} alt="" className="db w-100"/>
-
       <div className="bg-off-white pv4 ph3">
       	<div className="mw7 center">
 
