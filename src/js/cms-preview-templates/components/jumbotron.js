d97ad1614af1719127dd1b44990656ea63810de7
berespt <piotr.beres@gmail.com>
2018-02-09 23:48:54 +0100

    Change jumbotron text color

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -9,13 +9,13 @@ export default class Jumbotron extends React.Component {
       }}>
         <div className="mw7 center ph3">
           <div className="db mb3">
-            <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+            <div className="mw7 relative bg-fix-white-alpha mb3">
+              <h1 className="f2 f1-l b di lh-copy mb3 white mw6 bg-white-alpha">
                 { title }
               </h1>
             </div>
-            <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
+            <div className="mw7 relative bg-fix-white-alpha">
+              {subtitle && <p className="b f4 di lh-copy mb3 white mw6 bg-white-alpha">{ subtitle }</p>}
             </div>
           </div>
         </div>
