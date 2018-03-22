920f507a0e2c5daff0b0b255613cfce82e11f35d
Piotr Beres <piotr.beres@teamaol.com>
2018-03-22 22:41:23 +0100

    Add youtube video to home page

--- site/content/_index.md
+++ site/content/_index.md
@@ -8,6 +8,7 @@ blurb:
 intro:
     heading: "Kraków, 10-13 August"
     text: "Registration and schedule will come soon."
+    video: "https://www.youtube.com/embed/hm0WhUQ-UmE?rel=0&amp;showinfo=0"
 about:
     heading: About us
     text: Hummingbird Blues has been going on since 2015. It is a blues dancing
--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -10,11 +10,12 @@ export default class PostPreview extends React.Component {
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
-        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
 
-    return <div>
-        <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
+    return (
+      <div>
+        <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])} />
 
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
@@ -27,16 +28,25 @@ export default class PostPreview extends React.Component {
           <div className="ph3 mw7 center">
             <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
             <p className="mb4 mw6">{entry.getIn(["data", "intro", "text"])}</p>
-
-            <div className="tc">
-              <a href="#" className="btn raise">Register now!</a>
+            <section class="relative aspect-ratio aspect-ratio--16x9 overflow-hidden">
+              <iframe
+                src={entry.getIn(["data", "intro", "video"])}
+                class="absolute top-0 left-0 h-100 w-100"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen
+              />
+            </section>
+            <div className="tc pt3">
+              <a href="#" className="btn raise">
+                Register now!
+              </a>
             </div>
           </div>
         </div>
 
         <div className="bg-grey-1 pv4">
           <div className="ph3 mw7 center">
-
             <div className="flex-l mhn2-l">
               <div className="w-40-l ph2-l">
                 <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "about", "heading"])}</h2>
@@ -45,18 +55,18 @@ export default class PostPreview extends React.Component {
               </div>
 
               <div className="w-60-l ph2-l">
-                <img src={getAsset(entry.getIn(["data", "about", "image"]))} alt="" className="mb3"/>
+                <img src={getAsset(entry.getIn(["data", "about", "image"]))} alt="" className="mb3" />
               </div>
             </div>
 
             <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">Read more</a>
+              <a href="{{.buttonLink}}" className="btn raise">
+                Read more
+              </a>
             </div>
-
           </div>
         </div>
-
-
-    </div>
+      </div>
+    );
   }
 }
