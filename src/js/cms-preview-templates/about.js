eec298eae16a02e066adfe72722a4f493714693d
Piotr Beres <piotr.beres@teamaol.com>
2018-03-25 22:56:34 +0200

    Move about page first paragraph to new line, just after heading

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -37,9 +37,9 @@ export default class AboutPreview extends React.Component {
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
 
       <div className="bg-grey-1 pv4">
-        <div className="flex-l mhn1-l ph3 center mw7">
-          <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
-          <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>
+        <div className="ph3 center mw7">
+          <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "blurb", "heading"])}</h2>
+          <p className="mb0">{entry.getIn(["data", "blurb", "text"])}</p>
         </div>
       </div>
 
